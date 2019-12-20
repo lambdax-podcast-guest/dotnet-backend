61970fe60c74ad5a1381431bd921df958543e456
MisterC-Rogers <charles.rogers2024@gmail.com>
2019-12-20 10:34:15 -0600

    fixed the endpoints all work with postman now

--- Guests/Controllers/GuestsController.cs
+++ Guests/Controllers/GuestsController.cs
@@ -1,35 +1,98 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Guests.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Guests.Controllers{
     [Route("api/[controller]")]
     public class GuestsController : Controller
     {
 
-        readonly GuestsContext Context;
+        private readonly GuestsContext Context;
+
         public GuestsController(GuestsContext context)
             => Context = context;
 
+        // GET: api/guests
         [HttpGet]
-        public IActionResult GetGuest(){
-            
-            var guests = Context.Guests.ToList();
+        public async Task<ActionResult<IEnumerable<Guest>>> GetGuests()
+        {
 
+            var guests = await Context.Guests.ToListAsync();
+
+            if(guests.Count == 0){
+                return NotFound("Sorry to say but you have no guests...");
+            }
             return Ok(guests);
         }
 
+        // GET: api/guests/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<Guest>>> GetGuests(int id)
+        {
+
+            var guest = await Context.Guests.FindAsync(id);
+
+            if(guest == null){
+                return NotFound("No such guest found, please check again...");
+            }
+            return Ok(guest);
+        }
+
+        // Put: api/guests/2
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGuest(int id, Guest input){
+
+            var guest = await Context.Guests.FindAsync(id);
+
+            if(guest == null){
+                return NotFound("The guest you are looking for does not appear to be...");
+            }
+
+            guest.Name = input.Name;
+            guest.Email = input.Email;
+
+            await Context.SaveChangesAsync();
+
+            return Ok(guest);
+        }
+
+        // Post: api/guests
         [HttpPost]
-        public IActionResult CreateGuest(){
+        public async Task<IActionResult> CreateGuest(Guest input){
+
             var guest = new Guest(){
-                Name = "Charlie Rogers",
-                Email = "charles.rogers2024@gmail.com"
+                Name = input.Name,
+                Email = input.Email
             };
 
-            Context.Add(guest);
-            Context.SaveChanges();
+            Context.Guests.Add(guest);
+            await Context.SaveChangesAsync();
 
-            return Ok("Successfully created a new guest...");
+            return CreatedAtAction(nameof(GetGuests), new { id = guest.Id }, guest);
+        }
+
+        // Delete: api/guests/2
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGuest(int id){
+
+            var guest = await Context.Guests.FindAsync(id);
+
+            if(guest == null){
+                return NotFound();
+            }
+
+            Context.Remove(guest);
+
+            await Context.SaveChangesAsync();
+
+            if(Context.Guests == null){
+                return NotFound("No guests found");
+            }
+            return Ok(Context.Guests);
         }
     }
 }
--- Guests/Models/Guest.cs
+++ Guests/Models/Guest.cs
@@ -4,11 +4,16 @@ namespace Guests.Models
 {
     public class Guest
     {
-        public Guest() => Id = new Guid();
-        public Guid Id { get; set; }
+        
+        public int Id { get; set; }
 
         public string Name { get; set;}
 
         public string Email { get; set;}
+
+        internal object MapGuestsResponse()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

acbce710587609073e0e091a1a24bfb5ec94572a
clem9281 <clem9281@gmail.com>
2020-05-13 17:20:28 -0700

    changes guest controller to get guests from appuser table

--- Guests/Controllers/GuestsController.cs
+++ Guests/Controllers/GuestsController.cs
@@ -1,109 +1,92 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Guests.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using Microsoft.AspNetCore.Identity;
 
 namespace Guests.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     public class GuestsController : Controller
     {
 
         private readonly AppUserContext Context;
+        private UserManager<AppUser> _userManager;
 
-        public GuestsController(AppUserContext context)
-            => Context = context;
+        public GuestsController(AppUserContext context, UserManager<AppUser> userManager)
+        {
+            Context = context;
+            _userManager = userManager;
+        }
+
+        public class GuestOutput
+        {
+            public string Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public List<GuestTopic> Topics { get; set; }
+            // //if the guest is also a host, it would be nice to be able to show their podcasts in this view
+            public List<PodcastHost> Podcasts { get; set; }
+            public string AvatarUrl { get; set; }
+            public string Headline { get; set; }
+            public string Bio { get; set; }
+
+            public GuestOutput(AppUser guest)
+            {
+                Id = guest.Id;
+                FirstName = guest.FirstName;
+                LastName = guest.LastName;
+                Email = guest.Email;
+                Topics = guest.GuestTopics;
+                Podcasts = guest.PodcastHosts;
+                AvatarUrl = guest.AvatarUrl;
+                Headline = guest.HeadLine;
+                Bio = guest.Bio;
+            }
+        }
 
         // GET: api/guests
         [HttpGet]
-        [Authorize]
-        public async Task<ActionResult<IEnumerable<Guest>>> GetGuests()
+        public async Task<ActionResult<IEnumerable<GuestOutput>>> GetGuests()
         {
 
-            var guests = await Context.Guests.ToListAsync();
-
+            var guests = await _userManager.GetUsersInRoleAsync("Guest");
+            // If there are no guests don't bother normalizing the output
             if (guests.Count == 0)
             {
-                return NotFound("Sorry to say but you have no guests...");
+                return Ok("Sorry, there are no guests...");
             }
-            return Ok(guests);
+
+            List<GuestOutput> guestOutput = new List<GuestOutput>();
+            foreach (AppUser guest in guests)
+            {
+                guestOutput.Add(new GuestOutput(guest));
+            }
+
+            return Ok(guestOutput);
         }
 
         // GET: api/guests/1
         [HttpGet("{id}")]
-        public async Task<ActionResult<IEnumerable<Guest>>> GetGuests(int id)
+        public async Task<ActionResult<IEnumerable<GuestOutput>>> GetGuests(string id)
         {
 
-            var guest = await Context.Guests.FindAsync(id);
+            var guest = await _userManager.FindByIdAsync(id);
+            var roles = await _userManager.GetRolesAsync(guest);
+            bool isGuest = Array.Exists(roles.ToArray(), role => role == "Guest" || role == "guest");
 
-            if (guest == null)
+            if (guest == null || !isGuest)
             {
                 return NotFound("No such guest found, please check again...");
             }
-            return Ok(guest);
+            GuestOutput guestOutput = new GuestOutput(guest);
+            return Ok(guestOutput);
         }
 
-        // Put: api/guests/2
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutGuest(int id, [FromBody] Guest input)
-        {
-
-            var guest = await Context.Guests.FindAsync(id);
-
-            if (guest == null)
-            {
-                return NotFound("The guest you are looking for does not appear to be...");
-            }
-
-            guest.Name = input.Name;
-            guest.Email = input.Email;
-
-            await Context.SaveChangesAsync();
-
-            return Ok(guest);
-        }
-
-        // Post: api/guests
-        [HttpPost]
-        public async Task<IActionResult> CreateGuest([FromBody] Guest input)
-        {
-
-            var guest = new Guest()
-            {
-                Name = input.Name,
-                Email = input.Email
-            };
-
-            Context.Guests.Add(guest);
-            await Context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetGuests), new { id = guest.Id }, guest);
-        }
-
-        // Delete: api/guests/2
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteGuest(int id)
-        {
-
-            var guest = await Context.Guests.FindAsync(id);
-
-            if (guest == null)
-            {
-                return NotFound();
-            }
-
-            Context.Remove(guest);
-
-            await Context.SaveChangesAsync();
-
-            if (Context.Guests == null)
-            {
-                return NotFound("No guests found");
-            }
-            return Ok(Context.Guests);
-        }
     }
 }

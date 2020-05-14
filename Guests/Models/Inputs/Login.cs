d399ccd1045980be03fc98be9dfb0c0d8ca0ec35
clem9281 <clem9281@gmail.com>
2020-05-14 12:02:32 -0700

    adds proper validation to login and register input models

--- Guests/Models/Inputs/Login.cs
+++ Guests/Models/Inputs/Login.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 namespace Guests.Models.Inputs
 {
     public class LoginInput
     {
+        [Required]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
         public LoginInput() { }
     }

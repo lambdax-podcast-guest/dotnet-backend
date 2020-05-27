0db79745fa2b1fa87cc5c9733a57e376ba1807bf
clem9281 <clem9281@gmail.com>
2020-05-26 17:18:42 -0700

    begins testing login endpoint, adds test to ensure a user recieves a bad request response when they give the wrong password

--- Guests/Tests/LoginTests.cs
+++ Guests/Tests/LoginTests.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Xunit;
 using Xunit.Abstractions;
 using Guests.Models.Inputs;
@@ -12,39 +13,52 @@ namespace Guests.Tests
         }
 
         // -------------------------------------------------------------------------------------------------
-        /// <summary>Test that the login endpoint returns Unauthorized when the user logs in with an incorrect password</summary>
+        /// <summary>Test that the login endpoint returns bad request when the user logs in with an incorrect password</summary>
         // -------------------------------------------------------------------------------------------------
+        [Fact]
         public async void TestLoginRejectsBadPassword()
         {
             string[] roles = new string[] { "Guest" };
 
             // generate new unique user and register it
-            // TODO!! This logic should be wrapped up somewhere, it is heavilly repeated between register tests and login tests. Also since we are sharing db context among all the tests we need to be in strict control of what data we have called
             RegisterInput guestUser = fixture.accountHelper.GenerateUniqueRegisterModel(roles);
 
             // turn the register input into json and set the request headers
             var content = JsonHelper.CreatePostContent(guestUser);
 
             // get the response
-            HttpResponseMessage firstResponse = await fixture.httpClient.PostAsync("/api/account/register", content);
+            HttpResponseMessage response = await fixture.httpClient.PostAsync("/api/account/register", content);
 
-            // the first time we run the request the response should be successful
-            Assert.True(firstResponse.IsSuccessStatusCode);
+            // assert the response was successful
+            Assert.True(response.IsSuccessStatusCode);
 
+            // now we need a new login model. make one with a bad password
+            LoginInput loginInput = new LoginInput() { Email = guestUser.Email, Password = "BadPassword1!" };
+
+            // get the login Input as Json Content with headers
+            var loginContent = JsonHelper.CreatePostContent(loginInput);
+
+            // get the login response
+            HttpResponseMessage loginResponse = await fixture.httpClient.PostAsync("/api/account/login", loginContent);
+
+            // Assert the login response was not successful
+            Assert.False(loginResponse.IsSuccessStatusCode);
+
+            // Assert we got a bad request from the login response
+            Assert.Equal(HttpStatusCode.BadRequest, loginResponse.StatusCode);
         }
 
         // // -------------------------------------------------------------------------------------------------
         // /// <summary>Test the the login endpoing returns a token</summary>
         // // -------------------------------------------------------------------------------------------------
-
+        // [Fact]
         // public async void TestLoginReturnsAToken()
         // {
-
         // }
         // // -------------------------------------------------------------------------------------------------
         // /// <summary>Test that the login endpoint rejects requests that have missing fields</summary>
         // // -------------------------------------------------------------------------------------------------
-
+        // [Fact]
         // public async void TestLoginReturnsBadRequesOnInvalidModel()
         // {
 

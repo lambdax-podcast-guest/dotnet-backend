0e4034ad10769c4ebb65fb731519f7698b1c20d5
clem9281 <clem9281@gmail.com>
2020-05-29 11:58:00 -0700

    updates DeserializeResponseAndReturnErrors in JsonHelper to use TryDeserializeJson

--- Guests/Tests/Helpers/JsonHelper.cs
+++ Guests/Tests/Helpers/JsonHelper.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
@@ -12,6 +11,8 @@ namespace Guests.Tests
     public class JsonHelper
     {
         /// <summary>Turn any input class into json content for post</summary>
+        /// <typeparam name="T"> The type of the object to be created into JsonContent</typeparam>
+        /// <param name="genericObject">The object instance to be created into JsonContent</param>
         public static JsonContent CreatePostContent<T>(T genericObject)
         {
             // turn the register input into json and set the request headers
@@ -19,14 +20,24 @@ namespace Guests.Tests
             content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
             return content;
         }
+
         /// <summary>Deserialize bad request object, then deserialize it's errors object, and return the errors</summary>
+        /// <param name="response">HttpResponseMessage that can be deserialized into a CustomBadRequest object</param>
         public async static Task<Errors> DeserializeResponseAndReturnErrors(HttpResponseMessage response)
         {
             // deserialize the stream into what we expect the output to look like
-            CustomBadRequest responseObject = await JsonSerializer.DeserializeAsync<CustomBadRequest>(response.Content.ReadAsStreamAsync().Result);
+            CustomBadRequest responseObject = await TryDeserializeJson<CustomBadRequest>(response);
 
             // the errors string needs to be deserialized into an object
-            return JsonSerializer.Deserialize<Errors>(responseObject.errors.ToString());
+            // the deserialization may have worked even if errors weren't present on the response
+            // so if errors is null, this deserialize call will throw
+            // we will use fluent assertions here to make sure we get a clean error message
+            Func<Errors> deserializeErrors = () => JsonSerializer.Deserialize<Errors>(responseObject.errors.ToString());
+
+            // use fluent assertions to clearly show if something is wrong with this call to the serializer
+            deserializeErrors.Should().NotThrow("because we deserialized a bad request object and we expect there to be an errors field with a value present on that bad resquest. This assertion is made in the JsonHelper.");
+
+            return deserializeErrors();
         }
 
         /// <summary>Deserialize response message to a dynamic type, ensure the serialization was successful, and return the serialized content</summary>
@@ -34,16 +45,19 @@ namespace Guests.Tests
         /// <param name="response">The response to be deserialized</param>
         public async static Task<T> TryDeserializeJson<T>(HttpResponseMessage response)
         {
-            // so we will deserialize from a stream
+            // await the response as a string in order to deserialize it
             string responseAsString = await response.Content.ReadAsStringAsync();
 
-            Func<T> deserialize = () => JsonSerializer.Deserialize<T>(responseAsString);
+            // create a func that targets the deserialize method from JsonDeserializer
+            // this will allow us to use fluent assertions to create a clear error message if there is something wrong with the response or the type we are trying to deserialize to
+            Func<T> deserializeResponse = () => JsonSerializer.Deserialize<T>(responseAsString);
 
-            // ensure the serialization worked 
-            deserialize.Should().NotThrow("because we expect the serializer to work if the request was valid. This assertion is made in the JsonHelper");
+            // ensure the serialization worked using fluent assertions
+            // not sure if it is best practice to have an assertion out here in a helper, but we are going to use this deserialization a lot
+            deserializeResponse.Should().NotThrow("because we expect the serializer to work if the request was valid. This assertion is made in the JsonHelper.");
 
             // deserialize the stream into what we expect the output to look like
-            return deserialize();
+            return deserializeResponse();
         }
     }
 }

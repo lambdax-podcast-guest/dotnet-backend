be0c96fc717c711f37bfbe07f803cfb8003fe0f2
brellin <cekakid@gmail.com>
2020-06-03 17:14:58 -0500

    add podcast endpoint
    not sure if this is right

--- Guests/Controllers/PodcastsController.cs
+++ Guests/Controllers/PodcastsController.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Guests.Entities;
 using Guests.Helpers;
 using Guests.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -32,9 +34,43 @@ namespace Guests.Controllers
             // get user claim containing user's id
             string userId = User.Claims.First(c => c.Type == ClaimTypes.Name).Value;
             // get podcasts whose host(s)/guest(s) has user's id
-            var podcasts = _context.Podcasts.Where(pc => pc.PodcastHosts.Any(pch => pch.HostId == userId) || pc.PodcastGuests.Any(pcg => pcg.GuestId == userId));
+            var podcasts = _context.Podcasts.Where(
+                pc =>
+                pc.PodcastHosts.Any(pch => pch.HostId == userId) ||
+                pc.PodcastGuests.Any(pcg => pcg.GuestId == userId)
+            );
+            // if no podcasts exist for user, 
             if (podcasts.Count() == 0) { return NotFound("There are no podcasts assigned to you"); }
             return Ok(podcasts);
+        }
+
+        [Authorize(Roles = Role.Host)]
+        [HttpPost]
+        public async Task<ActionResult> PostPodcast([FromBody] Podcast input)
+        {
+            try
+            {
+                // Create new podcast with input information, not including hosts or guests
+                Podcast newPodcast = new Podcast() { Name = input.Name, Description = input.Description, HeadLine = input.HeadLine };
+                // Get id for user who is making new podcast
+                string userId = User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+                // Populate host list with user
+                List<PodcastHost> hostList = new List<PodcastHost>() { new PodcastHost() { HostId = userId, PodcastId = newPodcast.Id } };
+                // add hosts to podcast
+                newPodcast.PodcastHosts = hostList;
+                // initiate guest list
+                List<PodcastGuest> guestList = new List<PodcastGuest>();
+                // add each guest from input to guest list
+                foreach (string id in input.PodcastGuests.Select(g => g.GuestId)) { guestList.Add(new PodcastGuest() { GuestId = id, PodcastId = newPodcast.Id }); }
+                // add guests to podcast
+                newPodcast.PodcastGuests = guestList;
+                // save changes to db
+                await _context.SaveChangesAsync();
+                // return created if succeeded
+                return Created(newPodcast.Id.ToString(), newPodcast);
+            }
+            // return 500 if error occurred
+            catch (Exception) { return StatusCode(500); }
         }
     }
 }
